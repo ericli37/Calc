04bbd0a3def705bae6616cf91d00d4beed9274be
ericli37 <li.eric.w@gmail.com>
2025-05-02 11:12:23 -0500

    day 5

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ function clearDisplay() {
 
 function calculate() {
     try {
-        const expression = display.value.replace(/(\d+)\^(\d+)/g, 'Math.pow($1, $2)');
+        // EXPONENTIATION
+        let expression = display.value.replace(/(\d+)\^(\d+)/g, 'Math.pow($1, $2)');
+        
+        // TRIGONOMETRIC FUNCTIONS
+        expression = expression.replace(/sin\(([^)]+)\)/g, (_, angle) => Math.sin(toRadians(eval(angle))));
+        expression = expression.replace(/cos\(([^)]+)\)/g, (_, angle) => Math.cos(toRadians(eval(angle))));
+        expression = expression.replace(/cos\(([^)]+)\)/g, (_, angle) => Math.tan(toRadians(eval(angle))));
+
         display.value = eval(expression);
     } catch (error) {
         display.value = 'Error';
     }
+}
+
+// Helper function to convert degrees to radians
+function toRadians(degrees) {
+    return degrees * (Math.PI / 180);
 }
